8da486b72ba1e66c9eb8c711247b6661e9832e78
Gideon Spierings <gideongideon@gmail.com>
2018-11-08 22:25:04 +0100

    Ran prettier across entire project

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -3,10 +3,13 @@
 const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
-  on('file:preprocessor', webpack({
-    webpackOptions: require('@vue/cli-service/webpack.config'),
-    watchOptions: {},
-  }));
+  on(
+    'file:preprocessor',
+    webpack({
+      webpackOptions: require('@vue/cli-service/webpack.config'),
+      watchOptions: {},
+    })
+  );
 
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
